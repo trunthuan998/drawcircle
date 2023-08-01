27b0e915ad840387dfe1524887a34f45a7dcc090
trunthuan998 <trungthuankhoankhaithac@gmail.com>
2023-08-01 16:46:57 +0700

    ve hinh tron

---
+++ drawcircle.py
@@ -0,0 +1,52 @@
+import turtle
+
+#đặt kích thước viền cho hình tròn là 5
+turtle.pensize (5)
+ 
+#đặt màu sắc cho viền hình tròn là màu đỏ
+turtle.pencolor ("red")
+
+#đặt màu nền cho hình tròn là màu xanh
+turtle.fillcolor ("blue")
+turtle.begin_fill()
+
+#đặt bán kính của hình tròn là 100
+turtle.circle (100)
+turtle.end_fill()
+turtle.done
+
+import turtle
+
+#đặt kích thước viền cho hình tròn là 5
+turtle.pensize (5)
+
+#đặt màu sắc cho viền hình tròn là màu xanh
+turtle.pencolor ("blue")
+
+#for outer bigger circle
+#đặt màu nền cho hình tròn là màu đỏ
+turtle.fillcolor ("red")
+turtle.begin_fill()
+
+#đặt bán kính của hình tròn là 150
+turtle.circle (150)
+turtle.end_fill()
+turtle.done
+
+import turtle
+
+#đặt kích thước viền cho hình tròn là 5
+turtle.pensize (5)
+
+#đặt màu sắc cho viền hình tròn là màu đỏ
+turtle.pencolor ("red")
+
+#tuỳ chỉnh điểm bắt đầu vẽ hình tròn
+turtle.Turtle().goto(-40, 120)
+#đặt màu nền cho hình tròn là màu đỏ
+turtle.fillcolor ("red")
+turtle.begin_fill()
+
+#đặt bán kính của hình tròn là 150
+turtle.circle (150)
+turtle.end_fill()
